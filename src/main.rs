3e2396886704b140771fd33081c796e465d08728
Jint3x <65330793+Jint3x@users.noreply.github.com>
2021-08-02 06:28:02 +0300

    feat: File consistency
    A function has been added, which allows for a passed list (2 of them)
    of file paths to be compared and the inconsistencies are removed by
    either removing, adding or changing the files from the backup folder.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use env_plus;
 mod helpers;
-use helpers::{fetch_files, filter_files};
+use helpers::{DirItems, fetch_files, filter_files};
 
 fn main() {
     env_plus::EnvLoader::new()
@@ -26,13 +26,86 @@ fn main() {
     create_backup(mfolder, bfolder, to_filter)
 }
 
+
 /// The main function that creates a backup of the files in each folder.
 ///
 fn create_backup(mfolder: String, bfolder: String, to_filter: Vec<&str>) {
-    let mut mfiles = fetch_files(mfolder);
-    let mut bfiles = fetch_files(bfolder);
-    filter_files(&mut mfiles.dirs, &mut bfiles.dirs, to_filter);
+    let mut mfiles = fetch_files(mfolder.clone());
+    let mut bfiles = fetch_files(bfolder.clone());
+    filter_files(&mut mfiles.dirs, &mut bfiles.dirs, to_filter.clone());
+
+    // Go to the folder and clear all differences between the backup and the main dir.
+    // Then, a list of sub dirs will be given and the same function will be called on all of them.
+    clear_inconsistencies_files(mfiles.files, bfiles.files, mfolder.clone(), bfolder.clone());
+    let remaining_dirs = clear_inconsistencies_dirs(mfiles.dirs, bfiles.dirs);
+
+    for (main_dir, backup_dir) in remaining_dirs {
+        // create_backup(main_dir, backup_dir, to_filter.clone());
+    }
+}
+
+/// Clear inconsistencies between files:
+/// (M = mfolder, B = bfolder)
+/// 
+/// 1. If file exists in B, but doesn't exist in M, remove it.
+/// 2. If a file exists in M, but doesn't exist in B, copy it.
+/// 3. If file exists in both M and B, check it's metadata and copy it to B if there's a difference.
+fn clear_inconsistencies_files(mfiles: Vec<PathBuf>, bfiles: Vec<PathBuf>, mpath: String, bpath: String) {
+    let exists_in_m_only = mfiles.iter().filter(|mfile| {
+        bfiles.iter().all(|bfile| {
+            bfile.file_name().unwrap() != mfile.file_name().unwrap()
+        })
+    });
+
+    let exists_in_b_only = bfiles.iter().filter(|bfile| {
+        mfiles.iter().all(|mfile| {
+            mfile.file_name().unwrap() != bfile.file_name().unwrap()
+        })
+    });
+
+    let exists_in_m_and_b = bfiles.iter().filter_map(|bfile| {
+        for mfile in mfiles.iter() {
+            if mfile.file_name().unwrap() == bfile.file_name().unwrap() {
+                return Some((mfile, bfile))
+            }
+        }
+
+        None
+    });
+
+    // Handle error later
+    // Run step 1
+    exists_in_b_only.for_each(|file| {
+        std::fs::remove_file(file).unwrap()
+    });
+
+    // Run step 2
+    exists_in_m_only.for_each(|file| {
+        let bpathbuf = PathBuf::from(bpath.clone());
+        let bpathbuf = bpathbuf.join(file.file_name().unwrap());
+
+        std::fs::copy(file, bpathbuf).unwrap(); 
+    });
+
+    // Run step 3
+    exists_in_m_and_b.for_each(|(mfile, bfile)| {
+        let data_main = mfile.metadata().unwrap().modified().unwrap();
+        let data_backup = bfile.metadata().unwrap().modified().unwrap();
+
+        if data_main != data_backup {
+            std::fs::remove_file(bfile).unwrap();
+            std::fs::copy(mfile, bfile).unwrap();
+        }
+    })
+}
 
 
-    dbg!("{}, {}", mfiles, bfiles);
+/// Clear inconsistencies between dirs:
+/// (M = mfolder, B = bfolder)
+/// 
+/// 1. If dir exists in M, but doesn't exist in B, create it.
+/// 2. If dir exists in B, but doesn't exist in M, remove all of its contents and remove it.
+/// 3. If dir exists in both M and B, do nothing.  
+fn clear_inconsistencies_dirs(mdirs: Vec<PathBuf>, bdirs: Vec<PathBuf>) -> Vec<(String, String)> {
+    vec!(("".into(), "".into()))
 }
